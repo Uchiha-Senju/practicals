7f33776e99ff4a58cea757fdddea6186fd307d79
Uchiha-Senju <Uchiha-Senju@users.noreply.github.com>
2019-09-05 20:14:43 +0530

    fix file handling script in sir practicals

--- xii/sir_practical_scripts/ques_22.cpp
+++ xii/sir_practical_scripts/ques_22.cpp
@@ -27,28 +27,25 @@ bool isVowel(char c) {
       break;
   }
 }
-char readNthByteFromCurrentPosition(ifstream& file, int n) {
-  file.seekg(n, std::ios::cur);
-  char ch = file.get();
-  file.seekg(-n, std::ios::cur);
-  return ch;
-}
 
 int main() {
   char file_name[51];
+  ifstream input_file;
+  
   cout << "Enter file name (max 50 char) : "; cin.getline(file_name, 51, '\n');
-  ifstream input_file (file_name, ios::in);
+  input_file.open(file_name, ios::in);
   if (not input_file.is_open()) {
     cout << "ERR : File does not exist\n";
     return 0;
   }
-  input_file.seekg(0, ios::beg);
   
+  input_file.seekg(0, ios::beg);
+  long long file_length = -input_file.tellg();
   input_file.seekg(0, ios::end);
-  long long file_length = input_file.tellg();
+  file_length += input_file.tellg();
   input_file.seekg(0, ios::beg);  
   
-  char ch;
+  char ch, prev_ch;
   int no_of_blanks = 0, no_of_lines = 0, no_of_up = 0, no_of_low = 0, no_of_lines_start_up = 0, no_of_words = 0, no_of_digits = 0, no_of_words_end_vow = 0;
   bool word_started = false;
 
@@ -57,59 +54,48 @@ int main() {
   input_file.seekg(1, ios::beg);
   if (isUpperCase(ch)) {
     no_of_up++; word_started = true;
-    // if (readNthByteFromCurrentPosition(input_file, -1) == '\n') 
-      no_of_lines_start_up++;
+    no_of_lines_start_up++;
   } else if (isLowerCase(ch)) {
     no_of_low++; word_started = true;
   } else if (isDigit(ch)) {
     no_of_digits++;
-  } else if (ch == '\n') {
+  } else if (ch == '\n' or ch == '\r') {
     no_of_lines++;
-    // if (word_started)
-      // no_of_words++;
-    // word_started = false;
-    // if (isVowel( readNthByteFromCurrentPosition(input_file, -1) )) 
-      // no_of_words_end_vow++;
-    // else if (readNthByteFromCurrentPosition(input_file, -1) == '\n') 
-      no_of_blanks++;
-  // } else if (ch == ' ') {
-    // no_of_words++;
-    // if (isVowel( readNthByteFromCurrentPosition(input_file, -1) )) 
-      // no_of_words_end_vow++;
+    no_of_blanks++;
   }
   
   while (not input_file.eof()) {
-    input_file.get(&ch, 1);
-    input_file.seekg(1, ios::cur);
+    prev_ch = ch;
+    input_file.read(&ch, 1);
+    
     if (isUpperCase(ch)) {
       no_of_up++; word_started = true;
-      if (readNthByteFromCurrentPosition(input_file, -1) == '\n') 
+      if (prev_ch == '\n' or prev_ch == '\r') 
         no_of_lines_start_up++;
     } else if (isLowerCase(ch)) {
       no_of_low++; word_started = true;
     } else if (isDigit(ch)) {
       no_of_digits++;
-    } else if (ch == '\n') {
+    } else if (ch == '\n' or ch == '\r') {
       no_of_lines++;
       if (word_started)
         no_of_words++;
       word_started = false;
-      if (isVowel( readNthByteFromCurrentPosition(input_file, -1) )) 
+      if (isVowel( prev_ch )) 
         no_of_words_end_vow++;
-      else if (readNthByteFromCurrentPosition(input_file, -1) == '\n') 
+      else if (prev_ch == '\n' or prev_ch == '\r') 
         no_of_blanks++;
     } else if (ch == ' ') {
       if (word_started)
         no_of_words++;
       word_started = false;
-      if (isVowel( readNthByteFromCurrentPosition(input_file, -1) )) 
+      if (isVowel( prev_ch )) 
         no_of_words_end_vow++;
     } else if (not isAlphaNum(ch)) {
       if (word_started)
         no_of_words++;
       word_started = false;
     }
-    cout << "\n read char " << input_file.tellg() << " : " << ch;
   }
   input_file.clear();
 
